468e089bbfa18af4037b3646732538b651b5496a
JOYKIX <64356321+JOYKIX@users.noreply.github.com>
2024-08-23 00:25:46 +0200

    Update staff.js

--- js/staff.js
+++ js/staff.js
@@ -5,13 +5,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const staffMembers = [
         {
             name: "Joykix",
-            role: "Configuration du Serveur et Sélection des Mods",
-            bio: "Joykix est responsable de la configuration du serveur Minecraft et de la sélection des mods. Avec une attention méticuleuse aux détails, il assure que le serveur fonctionne de manière optimale et que les mods choisis enrichissent l'expérience de jeu."
+            role: "Flemme d'écrire",
+            bio: "Flemme d'écrire"
         },
         {
             name: "Pakoro",
-            role: "Sélection des Mods",
-            bio: "Pakoro collabore étroitement avec Joykix pour sélectionner les mods. Son expertise dans les mods permet de créer un environnement de jeu immersif et varié, apportant des fonctionnalités uniques au serveur."
+            role: "Flemme d'écrire",
+            bio: "Flemme d'écrire"
         }
     ];
 
